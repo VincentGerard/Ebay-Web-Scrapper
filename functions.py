49504ec193ead1585080906c7576578535523211
Vincent <mail.vincent.gerard@gmail.com>
2021-12-15 05:01:31 +0100

    Added Config file

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import datetime
+
 def	strToInt(string):
 	ret = 0
 	for letter in string:
@@ -6,3 +8,7 @@ def	strToInt(string):
 			ret = ret * 10
 			ret = ret + number
 	return ret
+
+def myPrint(*args):
+	#Add log
+    print("[" + str(datetime.datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss")) + "]" +" ".join(map(str,args)))
--- main.py
+++ main.py
@@ -1,63 +1,85 @@
+from http import cookiejar
+from time import sleep
 from bs4 import BeautifulSoup
 import io
 from bs4.dammit import html_meta
 import requests
 from functions import *
+import datetime
+import configparser
+import json
+from http.cookies import SimpleCookie
 
 """
 Max page = 50
-"seite:50" => Add page to search
+Cookies in congif files must be with ';' separator, juste copy paste google chrome cookies for best use
 """
 
 def main():
-	minPrice = 500
-	ebayUrl = "https://www.ebay-kleinanzeigen.de"
-	baseUrl = "https://www.ebay-kleinanzeigen.de/s-preis:"
-	endUrl = ":/marklin/k0"
-	url = baseUrl + str(minPrice) + endUrl
-	userAgent ="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
-	headers = { 'User-Agent' : userAgent}
-
-	htmlItem = "ad-listitem lazyload-item"
-	htmlDescription = "aditem-main--middle--description"
-	htmlPrice = "aditem-main--middle--price"
-	htmlEllipsis = "ellipsis"
-	htmlImage = "imagebox"
-
 	try:
-		print("URL = " + url)
-
-		result = requests.get(url,headers=headers)
-		print("Result = + " + str(result.status_code))
-
-		html_text = result.text
-
-		# Manual Testing when the website does not repond(to much spam?)
-		#html_text = open("test.html", 'r').read()
+		#Load Config
+		with open('config.json', 'r') as f:
+			config = json.load(f)
+		minPrice = config['minPrice']
 		
-		soup = BeautifulSoup(html_text, 'lxml')
-		print(html_text.count(htmlItem))
-		jobs = soup.find_all('li', class_ = htmlItem)
+		googleChromeCookie = config['googleChromeCookie']
+		cookie = SimpleCookie()
+		cookie.load(googleChromeCookie)
+		#Create custom cookie dictionairy
+		cookies = {}
+		for key, morsel in cookie.items():
+			cookies[key] = morsel.value
 
-		for job in jobs:
-			try:
-				price = strToInt(job.find("p",htmlPrice).text)
-				title = job.find("a", class_ = htmlEllipsis).text
-				urlLink = ebayUrl + job.find("a", class_ = htmlEllipsis)['href']
-				imageLink = job.find("div", class_ = htmlImage)['data-imgsrc']
-			except KeyError as k:
-				imageLink = ""
+		#Variables
+		pageNumber = 0
+		ebayUrl = "https://www.ebay-kleinanzeigen.de"
+		userAgent ="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+		headers = { 'User-Agent' : userAgent}
+
+		#HtmlSearchTags
+		htmlItem = "ad-listitem lazyload-item"
+		htmlDescription = "aditem-main--middle--description"
+		htmlPrice = "aditem-main--middle--price"
+		htmlEllipsis = "ellipsis"
+		htmlImage = "imagebox"
+
+		
+		
+		#Search all 50 pages
+		while pageNumber <= 50:
+			url = "https://www.ebay-kleinanzeigen.de/s-preis:" + str(minPrice) + ":/seite:" + str(pageNumber) +"/marklin/k0"
+			result = requests.get(url,headers=headers, cookies=cookies)
+			myPrint("URL = " + url)
+			myPrint("StatusCode = " + str(result.status_code))
+
+			html_text = result.text
+
+			# Manual Testing when the website does not repond(to much spam?)
+			#html_text = open("test.html", 'r').read()
 			
+			soup = BeautifulSoup(html_text, 'lxml')
+			myPrint("Item Number = " + str(html_text.count(htmlItem)))
+			jobs = soup.find_all('li', class_ = htmlItem)
 
-			print("\nTitle = " + title)
-			print("Price = " + str(price))
-			print("Url   = " + urlLink)
-			#print(imageLink)
-			print("Image = " + imageLink)
+			for job in jobs:
+				try:
+					price = strToInt(job.find("p",htmlPrice).text)
+					title = job.find("a", class_ = htmlEllipsis).text
+					urlLink = ebayUrl + job.find("a", class_ = htmlEllipsis)['href']
+					imageLink = job.find("div", class_ = htmlImage)['data-imgsrc']
+				except KeyError as k:
+					imageLink = ""
+				
+				myPrint("Page  = " + str(pageNumber))
+				myPrint("Title = " + title)
+				myPrint("Price = " + str(price))
+				myPrint("Url   = " + urlLink)
+				myPrint("Image = " + imageLink + "\n")
+			pageNumber = pageNumber + 1
+			sleep(5)
 	except Exception as e:
-		print("Error exception caught!")
-		print(e)
-
+			myPrint("Error exception caught!")
+			myPrint(e)
 
 if __name__ == "__main__":
 	main()
